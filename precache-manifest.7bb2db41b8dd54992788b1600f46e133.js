2564f2295b609503ce6c67610059a5db4793c41c
Vinod Chandak <vinniechandak@gmail.com>
2024-05-20 12:53:54 -0400

    Updates

---
+++ precache-manifest.7bb2db41b8dd54992788b1600f46e133.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "978f425e99e13a485f4c1f57f2ddb4c5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3626b0454bee31342c1b",
+    "url": "/static/css/2.ddfacf9b.chunk.css"
+  },
+  {
+    "revision": "0e71d7db523fe6c38329",
+    "url": "/static/css/main.67c652d7.chunk.css"
+  },
+  {
+    "revision": "3626b0454bee31342c1b",
+    "url": "/static/js/2.17103158.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.17103158.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0e71d7db523fe6c38329",
+    "url": "/static/js/main.4b0c49eb.chunk.js"
+  },
+  {
+    "revision": "e8379b6ab8a07b966043",
+    "url": "/static/js/runtime-main.c5e0ff30.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "39061a53b8b672bc0ed9ad094a1b7ffa",
+    "url": "/static/media/home.39061a53.jpeg"
+  }
+]);
